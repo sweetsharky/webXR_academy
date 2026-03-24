646210ddc4829d831c8394ac13aecb657adac042
sweetsharky <67016888+sweetsharky@users.noreply.github.com>
2026-03-24 16:48:06 +0100

    Kommentare hinzugefügt

--- wikingAR/js/scene.js
+++ wikingAR/js/scene.js
@@ -6,28 +6,36 @@
 
 import * as THREE from 'three';
 
-export let camera, scene, renderer;
+export let camera, scene, renderer; //werden in andere Modulen benötigt. Light nicht, deswegen steht es nur 1x in der Funktion und, da sie nie überschrieben werden muss = const und kein let.
 export const clock = new THREE.Clock();
 
 export function initScene() {
-  const container = document.createElement('div');
-  document.body.appendChild(container);
+  const container = document.createElement('div'); //1. div-Tag wird in HTML-document initialisiert, genannt "container"(=einfach nur der Name für diesen Wrapper)
+  document.body.appendChild(container); //2. der div-Tag (=container) wird an den Body der HTML-Seite gehängt
 
   scene = new THREE.Scene();
 
-  camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 40);
+  camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 40); // der Browser weiß jederzeit was window ist, da es Teil der Web-API ist = globale Namespace.--> ist die (u.a.) Größe des Browserfensters, das der Nutzer öffnet mit innerWidth und innerHeight)
   scene.add(camera); // wichtig: Kamera in die Szene hängen, damit über camera.add(modelname) Kinder gerendert werden
 
+  const light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
+  light.position.set(0.5, 1, 0.25);
+  scene.add(light);
+  
   renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.xr.enabled = true; // we have to enable the renderer for webxr
-  container.appendChild(renderer.domElement);
+  renderer.xr.enabled = true; // muss für webXR so eingestellt werden, damit der Renderer XR ermöglicht, denke ich!
+  container.appendChild(renderer.domElement); // 3. an den Div-container wird ein WebGLRenderer angehängt --> es wird automatisch ein <canva>-Tag geöffnet innerhalb des <div>! (da WebGLRenderer ein HTMLCanvasElemet ist)
+  renderer.domElement.classList.add('three.js-canvas'); //4. damit wir über CSS diesen Canvas anpassen können benennen wir ihn mit einer referenz "three.js-canvas" --> three.js Canvas soll in den Vordergrund für Touch-Interaktion 
+  /*
+  Durch Schritte 1-5. erreichen wir dies in der HTML-Datei:
 
-  // Canvas in den Vordergrund für Touch-Interaktion (CSS muss Klasse definieren)
-  renderer.domElement.classList.add('three.js-canvas');
+      <body>
+        <div>
+            <canvas class="three.js-canvas"></canvas>
+        </div>
+      </body>
 
-  var light = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 1);
-  light.position.set(0.5, 1, 0.25);
-  scene.add(light);
+  */
 }
